9aaae8c8cbcd31cfa21d69c180169105f71f3003
Yury Tarasov <90278949+yurytarasov@users.noreply.github.com>
2022-02-06 12:29:27 +0400

    добавил примеры

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ConsoleApp
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names = new string[13];
+
+            names[0] = "Ivan";
+            names[1] = "Maks";
+            names[2] = "Min";
+            names[3] = "IKolz";
+            names[4] = "Nina";
+            names[5] = "Sergey";
+           
+            Console.WriteLine("Введите Ваше имя");
+            names[6] = Console.ReadLine();
+
+            for (int i = 0; i < names.Length; i++)
+            {
+                Console.WriteLine(names[i]);
+            }           
+        }
+    }
+}
+
